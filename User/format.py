92a38d24ac41b898a5756415c847a19ffaf3d27f
liangjia <liangjia@variflight.com>
2019-10-10 11:12:40 +0800

    feat:add js css syntax

--- User/format.py
+++ User/format.py
@@ -6,7 +6,13 @@ class FormatCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		syntax = self.view.settings().get('syntax')
 
-		if syntax == 'Packages/HTML/HTML.sublime-syntax':
+		fore_end = [
+			'Packages/HTML/HTML.sublime-syntax',
+			'Packages/JavaScript/JavaScript.sublime-syntax',
+			'Packages/CSS/CSS.sublime-syntax'
+		]
+
+		if syntax in fore_end:
 			self.view.run_command('htmlprettify')
 			return None
 
